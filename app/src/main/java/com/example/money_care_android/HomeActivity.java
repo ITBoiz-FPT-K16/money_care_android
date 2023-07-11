0d73011a7d64951359582d5de651578ee82523d1
husone <sonhvnde160056@fpt.edu.vn>
2023-07-11 21:46:04 +0700

    delete unused import

--- app/src/main/java/com/example/money_care_android/HomeActivity.java
+++ app/src/main/java/com/example/money_care_android/HomeActivity.java
@@ -1,25 +1,19 @@
 package com.example.money_care_android;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 
 public class HomeActivity extends AppCompatActivity {
 

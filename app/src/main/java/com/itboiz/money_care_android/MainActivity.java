ad27f5ee3167c805612ff277c8845f43be556793
quangpao <ddquangbao@gmail.com>
2023-07-11 19:27:58 +0700

    Add UI (UI only, it relate Java file won't be added)

---
+++ app/src/main/java/com/itboiz/money_care_android/MainActivity.java
@@ -0,0 +1,27 @@
+package com.itboiz.money_care_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        } else {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+    }
+}
